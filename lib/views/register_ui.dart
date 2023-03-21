5c27315f8fe510085256b1a75a2df57a436291f9
MassamanGai <s645241004@sau.ac.th>
2023-03-21 15:06:37 +0700

    V.2 Created By : Aunop Chinnapong

---
+++ lib/views/register_ui.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors, unnecessary_import, implementation_imports, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RegisterUI extends StatefulWidget {
+  const RegisterUI({super.key});
+
+  @override
+  State<RegisterUI> createState() => _RegisterUIState();
+}
+
+class _RegisterUIState extends State<RegisterUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
