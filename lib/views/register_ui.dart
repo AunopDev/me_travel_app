efc1c6dc17aac090dab58c3d76b4c67c14828403
MassamanGai <s6452410004@sau.ac.th>
2023-03-28 16:02:06 +0700

    V.4 Created By : Aunop Chinnapong

--- lib/views/register_ui.dart
+++ lib/views/register_ui.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RegisterUI extends StatefulWidget {
   const RegisterUI({super.key});
@@ -14,6 +16,55 @@ class RegisterUI extends StatefulWidget {
 class _RegisterUIState extends State<RegisterUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text(
+          'ลงทะเบียนเข้าใช้งาน',
+          style: GoogleFonts.itim(),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    radius: MediaQuery.of(context).size.width * 0.2,
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.cameraRetro,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
